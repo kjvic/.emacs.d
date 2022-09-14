bd5398aedc3a1fa3b2ace4a620dd7330dd3df195
Kris Victorsen <xtfv@protonmail.com>
2022-09-14 11:23:44 -0700

    Import

--- init.el
+++ init.el
@@ -1,41 +1,48 @@
 ;; -*-no-byte-compile: t; -*-
-(when (version< emacs-version "24")
-  (unless (yes-or-no-p "This Emacs is old, config probably won't work. Kill? ")
-    (kill-emacs)))
+;;(when (version< emacs-version "24")
+;;  (unless (yes-or-no-p "This Emacs is old, config probably won't work. Kill? ")
+;;    (kill-emacs)))
 ;; ^^ Don't bother loading if your emacs is too old
 
 ;; Always load newest byte code
-(setq load-prefer-newer t)
+;;(setq load-prefer-newer t)
+;; Increase the garbage collection threshold to 50 MB to ease startup
+;;(setq gc-cons-threshold (* 50 1024 1024))
 
-;; Increase the garbage collection threshold to 500 MB to ease startup
-(setq gc-cons-threshold (* 500 1024 1024))
+;; https://jeffkreeftmeijer.com/emacs-straight-use-package/
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Packages
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (setq package-archives
-        '(("gnu" . "https://elpa.gnu.org/packages/")
-          ("melpa" . "https://melpa.org/packages/")
-          ("org" . "http://orgmode.org/elpa/")))
-  (package-initialize)
-  ;; Add org to package path
-  (unless (package-installed-p 'org)
-    (package-refresh-contents)
-    (package-install 'org)))
-(require 'org)
+;; Install use-package
+(straight-use-package 'use-package)
+
+;; Configure use-package to use straight.el by default
+(use-package straight
+             :custom (straight-use-package-by-default t))
+
+(use-package org)
 
 ;; Source: https://stackoverflow.com/a/8902202
-(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
+;;(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
 
 ;; Tangle code and load from my config.org file
-(setq krista/bootstrap-org-file "config.org")
-(setq krista/bootstrap-org-path
+(setq kris/bootstrap-org-file "config.org")
+(setq kris/bootstrap-org-path
       (expand-file-name
-       (concat user-emacs-directory krista/bootstrap-org-file)))
-
-
+       (concat user-emacs-directory kris/bootstrap-org-file)))
 ;; Tangle all of my source blocks together, and then load the result
-(org-babel-load-file krista/bootstrap-org-path)
+(org-babel-load-file kris/bootstrap-org-path)
 
 ;; Decrease garbage collection threshold to 5 MB
 ;; (add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 5 1024 1024))))
---
+++ nuke-compiled.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+set -euv
+set -o pipefail
+
+rm -f ./*.elc
+rm -f config.el
+rm -f literate/*.elc
+rm -f literate/*.el
